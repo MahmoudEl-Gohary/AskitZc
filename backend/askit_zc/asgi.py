eda603d0e642ee08a3f3b55d2d80277cf746d2c8
Mahmoud ElGohary <mahmoudelgohary.work@gmail.com>
2024-11-29 14:57:43 +0200

    Creating Django Project

---
+++ backend/askit_zc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for askit_zc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askit_zc.settings')
+
+application = get_asgi_application()
