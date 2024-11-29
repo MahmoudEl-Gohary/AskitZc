fd436a9bb158d7c3d1dc5d9caa62e4762dc28991
Mahmoud ElGohary <mahmoudelgohary.work@gmail.com>
2024-11-29 16:09:15 +0200

    adding the tailwind css configurations

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // Updated to 'content'
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
